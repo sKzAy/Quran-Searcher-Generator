40219f8ac10641419f848694027c683b61885962
sKzAy <1mkzaexe@gmail.com>
2024-10-05 21:59:34 +0500

    some media query things

--- components/Search.js
+++ components/Search.js
@@ -40,8 +40,10 @@ const Search = () => {
 </svg><input ref={InRef} type="text" placeholder='Enter Ayah Number' /></div>
        <div className="btn"><button onClick={handleClick}>Search</button></div>
        <div className="Result">
-       <div><b>Result</b>: <br />{Ayat.data.text}</div>
-       <div>{Ayat.data.surah.englishName} : {Ayat.data.numberInSurah}</div>
+       <p><b>Result</b>: <br />'{Ayat.data.text}'
+       <br />
+       <div>{Ayat.data.surah.englishName} : {Ayat.data.numberInSurah}</div></p>
+       
        </div>
     </div>
     
